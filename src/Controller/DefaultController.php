2c027d5c2fc0feb5416fba07a353c30a1252c242
Tomasz Lipowiec <t.lipowiec@lemonmind.com>
2024-01-23 23:48:11 +0100

    Order, data format and api-key

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -13,18 +13,18 @@ class DefaultController extends AbstractController{
     #[Route('/distance-sensor', name: 'distance-sensor-list', methods: ['GET'])]
     public function index(EntityManagerInterface $entityManager): Response{
 
-        $distances = $entityManager->getRepository(Distance::class)->findAll();
+        $distances = $entityManager->getRepository(Distance::class)->findBy([], ['id' => 'DESC']);
 
         return $this->render('distance/list.html.twig', ['distances' => $distances]);
     }
 
     #[Route('/api/save/{dist}/{key}', name: 'distance-sensor-save', methods: ['GET'])]
-    public function apiSave(EntityManagerInterface $entityManager, int $dist, string $key):  Response{
+    public function apiSave(EntityManagerInterface $entityManager, string $dist, string $key):  Response{
 
         if($key == "4a99c6e3-accc-465b-90bc-e6f3f3a892e8"){
             $distance = new Distance;
 
-            $distance->setCm($dist);
+            $distance->setCm((int)$dist);
             $distance->setDate(new DateTime('now'));
 
             $entityManager->persist($distance);
